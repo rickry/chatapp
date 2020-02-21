865230e65e256cf85c44e6fe51ec1eeac6d38dd7
rickry <r.stoit@gmail.com>
2020-02-21 15:07:36 +0100

    oops

--- app/Http/Controllers/Api/MessagesController.php
+++ app/Http/Controllers/Api/MessagesController.php
@@ -24,6 +24,7 @@ class MessagesController extends Controller
     public function byDate($date)
     {
         $messages = Messages::where('created_at', '>=', $date)->get();
+        $messages->load("user");
         return response()->json($messages);
     }
 
